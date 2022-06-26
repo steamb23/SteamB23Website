d0acc0a837e400838eb5814b1eff91d5c32042d4
SteamB23 <steamb23@outlook.com>
2022-06-27 01:21:46 +0900

    스크립트 실행 순서 수정

--- SteamB23Website/wwwroot/js/site.js
+++ SteamB23Website/wwwroot/js/site.js
@@ -3,127 +3,129 @@
 
 // Write your JavaScript code.
 
-window.addEventListener("load", (event) => {
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOMContentLoaded.");
     document.querySelectorAll(".preload").forEach((item, i, list) => {
         item.classList.remove("preload");
     });
 
-    const nav = document.querySelector("nav");
-    const menuOpen = nav.querySelector(".menu-open");
-    //let menuClose = nav.querySelector(".menu-close");
+    const navElement = document.querySelector("nav");
+    const menuOpen = navElement.querySelector(".menu-open");
+    //let menuClose = navElement.querySelector(".menu-close");
     const menuToggleEventHandler = (event) => {
-        nav.classList.toggle("toggle");
+        navElement.classList.toggle("toggle");
     };
     menuOpen.addEventListener("click", menuToggleEventHandler);
     //menuClose.addEventListener("click", menuToggleEventHandler);
-});
 
-function nav() {
-    const sections = document.querySelectorAll("section");
-    const nav = document.querySelector("nav");
-    const navLi = nav.querySelectorAll("ul li a");
-    window.addEventListener("scroll", (event) => {
-        let current = "";
 
-        sections.forEach((section) => {
-            const sectionTop = section.offsetTop;
-            if (pageYOffset >= sectionTop - innerHeight / 2) {
-                current = section.getAttribute("id");
-            }
-        })
+    function nav() {
+        const sections = document.querySelectorAll("section");
+        const nav = document.querySelector("nav");
+        const navLi = nav.querySelectorAll("ul li a");
+        window.addEventListener("scroll", (event) => {
+            let current = "";
 
-        navLi.forEach((li) => {
-            li.classList.remove("active");
-            if (li.getAttribute("href").includes(current)) {
-                li.classList.add("active");
-            }
-        });
-    });
-
-    if (window.innerWidth > 1500) {
-        nav.classList.add("toggle");
-    }
-}
-nav();
-
-function dragNav() {
-    let nav = document.querySelector("nav");
-    let pos = 0, delta = 0;
-    let drag = false;
-
-    function mouseDown(event, touch = null) {
-        if (nav.offsetHeight < window.innerHeight)
-            return;
-        if (event.preventDefault)
-            event.preventDefault();
-        pos = event.clientY;
-        drag = true;
-    }
-
-    function mouseMove(event, touch = null) {
-        if (!drag)
-            return;
-        if (event.preventDefault)
-            event.preventDefault();
-        delta = pos - event.clientY;
-        pos = event.clientY;
-        let targetTop = (nav.offsetTop - delta);
-        targetTop = Math.max(window.innerHeight - nav.offsetHeight - 20, targetTop);
-        targetTop = Math.min(20, targetTop);
-        nav.style.top = targetTop + "px";
-        nav.style.cursor = "move";
-    }
-
-    function mouseUp(event) {
-        if (!drag)
-            return;
-        nav.style.cursor = null;
-        pos = delta = 0;
-        drag = false;
-    }
-    nav.addEventListener("mousedown", mouseDown);
-    document.addEventListener("mousemove", mouseMove);
-    document.addEventListener("mouseup", mouseUp);
-    nav.addEventListener("touchstart", event => {
-        mouseDown(event.touches[0], event)
-    }, { passive: false });
-    document.addEventListener("touchmove", event => {
-        mouseMove(event.touches[0], event)
-    }, { passive: false });
-    document.addEventListener("touchend", event => {
-        mouseUp(event.touches[0])
-    });
-    window.addEventListener("resize", event => {
-        // 리셋
-        if (nav.offsetHeight < window.innerHeight)
-            nav.style.top = 20 + "px";
-    });
-}
-dragNav();
-
-function collapse() {
-    const toggleButtons = document.querySelectorAll("[data-toggle='collapse']");
-    toggleButtons.forEach((toggleButton) => {
-        toggleButton.addEventListener("click", (event) => {
-            const toggleTargetSelector = toggleButton.dataset.toggleTarget;
-
-            /** @type {Element} */
-            let toggleTarget = null;
-            if (toggleTargetSelector == null) {
-                toggleTarget = toggleButton.querySelector(".collapse,.collapsing");
-                if (toggleTarget == null) {
-                    toggleTarget = toggleButton.nextElementSibling;
-                    while (!toggleTarget.classList.contains("collapse,.collapsing")) {
-                        toggleTarget = toggleTarget.nextElementSibling;
-                    }
+            sections.forEach((section) => {
+                const sectionTop = section.offsetTop;
+                if (pageYOffset >= sectionTop - innerHeight / 2) {
+                    current = section.getAttribute("id");
                 }
-            } else {
-                toggleTarget = document.querySelector(toggleTargetSelector);
-            }
+            })
 
-            const bsCollapse = new bootstrap.Collapse(toggleTarget);
-            bsCollapse.toggle();
+            navLi.forEach((li) => {
+                li.classList.remove("active");
+                if (li.getAttribute("href").includes(current)) {
+                    li.classList.add("active");
+                }
+            });
         });
-    });
-}
-collapse();
+
+        if (window.innerWidth > 1500) {
+            nav.classList.add("toggle");
+        }
+    }
+    nav();
+
+    function dragNav() {
+        let nav = document.querySelector("nav");
+        let pos = 0, delta = 0;
+        let drag = false;
+
+        function mouseDown(event, touch = null) {
+            if (nav.offsetHeight < window.innerHeight)
+                return;
+            if (event.preventDefault)
+                event.preventDefault();
+            pos = event.clientY;
+            drag = true;
+        }
+
+        function mouseMove(event, touch = null) {
+            if (!drag)
+                return;
+            if (event.preventDefault)
+                event.preventDefault();
+            delta = pos - event.clientY;
+            pos = event.clientY;
+            let targetTop = (nav.offsetTop - delta);
+            targetTop = Math.max(window.innerHeight - nav.offsetHeight - 20, targetTop);
+            targetTop = Math.min(20, targetTop);
+            nav.style.top = targetTop + "px";
+            nav.style.cursor = "move";
+        }
+
+        function mouseUp(event) {
+            if (!drag)
+                return;
+            nav.style.cursor = null;
+            pos = delta = 0;
+            drag = false;
+        }
+        nav.addEventListener("mousedown", mouseDown);
+        document.addEventListener("mousemove", mouseMove);
+        document.addEventListener("mouseup", mouseUp);
+        nav.addEventListener("touchstart", event => {
+            mouseDown(event.touches[0], event)
+        }, { passive: false });
+        document.addEventListener("touchmove", event => {
+            mouseMove(event.touches[0], event)
+        }, { passive: false });
+        document.addEventListener("touchend", event => {
+            mouseUp(event.touches[0])
+        });
+        window.addEventListener("resize", event => {
+            // 리셋
+            if (nav.offsetHeight < window.innerHeight)
+                nav.style.top = 20 + "px";
+        });
+    }
+    dragNav();
+
+    function collapse() {
+        const toggleButtons = document.querySelectorAll("[data-toggle='collapse']");
+        toggleButtons.forEach((toggleButton) => {
+            toggleButton.addEventListener("click", (event) => {
+                const toggleTargetSelector = toggleButton.dataset.toggleTarget;
+
+                /** @type {Element} */
+                let toggleTarget = null;
+                if (toggleTargetSelector == null) {
+                    toggleTarget = toggleButton.querySelector(".collapse,.collapsing");
+                    if (toggleTarget == null) {
+                        toggleTarget = toggleButton.nextElementSibling;
+                        while (!toggleTarget.classList.contains("collapse,.collapsing")) {
+                            toggleTarget = toggleTarget.nextElementSibling;
+                        }
+                    }
+                } else {
+                    toggleTarget = document.querySelector(toggleTargetSelector);
+                }
+
+                const bsCollapse = new bootstrap.Collapse(toggleTarget);
+                bsCollapse.toggle();
+            });
+        });
+    }
+    collapse();
+});
